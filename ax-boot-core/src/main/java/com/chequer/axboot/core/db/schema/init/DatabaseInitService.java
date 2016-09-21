9ac5d122eb015572963a77b5af0e3654501a5c2f
Thomas Jang <tom@axisj.com>
2016-09-21 21:46:10 +0900

    기본 데이터 변경 테스트

--- ax-boot-core/src/main/java/com/chequer/axboot/core/db/schema/init/DatabaseInitService.java
+++ ax-boot-core/src/main/java/com/chequer/axboot/core/db/schema/init/DatabaseInitService.java
@@ -187,63 +187,63 @@ public class DatabaseInitService {
         userAuth.setGrpAuthCd("S0001");
         userAuthService.save(userAuth);
 
-        programService.save(Program.of("api","API","/swagger/","_self","N","Y","Y","N","N","N","N","N","N","N"));
-        programService.save(Program.of("login","로그인","/jsp/login.jsp","_self","N","N","N","N","N","N","N","N","N","N"));
-        programService.save(Program.of("main","메인","/jsp/main.jsp","_self","N","N","N","N","N","N","N","N","N","N"));
-        programService.save(Program.of("system-config-common-code","공통코드관리","/jsp/system/system-config-common-code.jsp","_self","Y","Y","Y","Y","N","N","N","N","N","N"));
-        programService.save(Program.of("system-config-menu","메뉴관리","/jsp/system/system-config-menu.jsp","_self","Y","Y","Y","N","N","N","N","N","N","N"));
-        programService.save(Program.of("system-config-program","프로그램관리","/jsp/system/system-config-program.jsp","_self","Y","Y","Y","N","N","N","N","N","N","N"));
-        programService.save(Program.of("system-auth-user","사용자관리","/jsp/system/system-auth-user.jsp","_self","Y","Y","Y","N","N","N","N","N","N","N"));
-        programService.save(Program.of("system-operation-log","에러로그 조회","/jsp/system/system-operation-log.jsp","_self","Y","Y","N","N","N","Y","N","N","N","N"));
+        programService.save(Program.of("api", "API", "/swagger/", "_self", "N", "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("login", "로그인", "/jsp/login.jsp", "_self", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("main", "메인", "/jsp/main.jsp", "_self", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("system-config-common-code", "공통코드관리", "/jsp/system/system-config-common-code.jsp", "_self", "Y", "Y", "Y", "Y", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("system-config-menu", "메뉴관리", "/jsp/system/system-config-menu.jsp", "_self", "Y", "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("system-config-program", "프로그램관리", "/jsp/system/system-config-program.jsp", "_self", "Y", "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("system-auth-user", "사용자관리", "/jsp/system/system-auth-user.jsp", "_self", "Y", "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        programService.save(Program.of("system-operation-log", "에러로그 조회", "/jsp/system/system-operation-log.jsp", "_self", "Y", "Y", "N", "N", "N", "Y", "N", "N", "N", "N"));
 
 
-        menuService.save(Menu.of(1L,"SYSTEM_MANAGER","시스템관리",nullL, 0, 0, "null"));
-        menuService.save(Menu.of(2L,"SYSTEM_MANAGER","공통코드 관리",1L, 1, 0, "system-config-common-code"));
-        menuService.save(Menu.of(3L,"SYSTEM_MANAGER","프로그램 관리",1L, 1, 1, "system-config-program"));
-        menuService.save(Menu.of(4L,"SYSTEM_MANAGER","메뉴관리",1L, 1, 2, "system-config-menu"));
-        menuService.save(Menu.of(5L,"SYSTEM_MANAGER","사용자 관리",1L, 1, 3, "system-auth-user"));
-        menuService.save(Menu.of(6L,"SYSTEM_MANAGER","에러로그 조회",1L, 1, 4, "system-operation-log"));
-        menuService.save(Menu.of(7L,"SYSTEM_MANAGER","레이아웃 샘플",nullL, 0, 1, "null"));
-        menuService.save(Menu.of(8L,"SYSTEM_MANAGER","API",nullL, 0, 2, "null"));
-        menuService.save(Menu.of(33L,"SYSTEM_MANAGER","검색&그리드",7L, 1, 0, "null"));
-        menuService.save(Menu.of(34L,"SYSTEM_MANAGER","검색&그리드(인라인에디트)",7L, 1, 1, "null"));
-        menuService.save(Menu.of(35L,"SYSTEM_MANAGER","검색&그리드(페이징)",7L, 1, 2, "null"));
-        menuService.save(Menu.of(36L,"SYSTEM_MANAGER","그리드&폼(좌우)",7L, 1, 3, "null"));
-        menuService.save(Menu.of(37L,"SYSTEM_MANAGER","그리드&폼(좌우,모달)",7L, 1, 4, "null"));
-        menuService.save(Menu.of(38L,"SYSTEM_MANAGER","그리드&폼(상하)",7L, 1, 5, "null"));
-        menuService.save(Menu.of(39L,"SYSTEM_MANAGER","그리드&탭폼(상하)",7L, 1, 6, "null"));
-        menuService.save(Menu.of(40L,"SYSTEM_MANAGER","트리&폼(좌우)",7L, 1, 7, "null"));
-        menuService.save(Menu.of(41L,"SYSTEM_MANAGER","AXBOOT.js",8L, 1, 0, "null"));
-        menuService.save(Menu.of(42L,"SYSTEM_MANAGER","AXBOOT.css",8L, 1, 1, "null"));
+        menuService.save(Menu.of(1L, "SYSTEM_MANAGER", "시스템관리", null, 0, 0, null));
+        menuService.save(Menu.of(2L, "SYSTEM_MANAGER", "공통코드 관리", 1L, 1, 0, "system-config-common-code"));
+        menuService.save(Menu.of(3L, "SYSTEM_MANAGER", "프로그램 관리", 1L, 1, 1, "system-config-program"));
+        menuService.save(Menu.of(4L, "SYSTEM_MANAGER", "메뉴관리", 1L, 1, 2, "system-config-menu"));
+        menuService.save(Menu.of(5L, "SYSTEM_MANAGER", "사용자 관리", 1L, 1, 3, "system-auth-user"));
+        menuService.save(Menu.of(6L, "SYSTEM_MANAGER", "에러로그 조회", 1L, 1, 4, "system-operation-log"));
+        menuService.save(Menu.of(7L, "SYSTEM_MANAGER", "레이아웃 샘플", null, 0, 1, null));
+        menuService.save(Menu.of(8L, "SYSTEM_MANAGER", "API", null, 0, 2, null));
+        menuService.save(Menu.of(33L, "SYSTEM_MANAGER", "검색&그리드", 7L, 1, 0, null));
+        menuService.save(Menu.of(34L, "SYSTEM_MANAGER", "검색&그리드(인라인에디트)", 7L, 1, 1, null));
+        menuService.save(Menu.of(35L, "SYSTEM_MANAGER", "검색&그리드(페이징)", 7L, 1, 2, null));
+        menuService.save(Menu.of(36L, "SYSTEM_MANAGER", "그리드&폼(좌우)", 7L, 1, 3, null));
+        menuService.save(Menu.of(37L, "SYSTEM_MANAGER", "그리드&폼(좌우,모달)", 7L, 1, 4, null));
+        menuService.save(Menu.of(38L, "SYSTEM_MANAGER", "그리드&폼(상하)", 7L, 1, 5, null));
+        menuService.save(Menu.of(39L, "SYSTEM_MANAGER", "그리드&탭폼(상하)", 7L, 1, 6, null));
+        menuService.save(Menu.of(40L, "SYSTEM_MANAGER", "트리&폼(좌우)", 7L, 1, 7, null));
+        menuService.save(Menu.of(41L, "SYSTEM_MANAGER", "AXBOOT.js", 8L, 1, 0, null));
+        menuService.save(Menu.of(42L, "SYSTEM_MANAGER", "AXBOOT.css", 8L, 1, 1, null));
 
 
-        commonCodeService.save(CommonCode.of("USER_STATUS","계정상태","ACCOUNT_LOCK","잠김",2));
-        commonCodeService.save(CommonCode.of("USER_ROLE","사용자 롤","API","API 접근 롤",6));
-        commonCodeService.save(CommonCode.of("USER_ROLE","사용자 롤","ASP_ACCESS","관리시스템 접근 롤",1));
-        commonCodeService.save(CommonCode.of("USER_ROLE","사용자 롤","ASP_MANAGER","일반괸리자 롤",3));
-        commonCodeService.save(CommonCode.of("LOCALE","로케일","en_US","미국",2));
-        commonCodeService.save(CommonCode.of("LOCALE","로케일","ko_KR","대한민국",1));
-        commonCodeService.save(CommonCode.of("DEL_YN","삭제여부","N","미삭제",1));
-        commonCodeService.save(CommonCode.of("USE_YN","사용여부","N","사용안함",2));
-        commonCodeService.save(CommonCode.of("USER_STATUS","계정상태","NORMAL","활성",1));
-        commonCodeService.save(CommonCode.of("AUTH_GROUP","권한그룹","S0001","시스템관리자 그룹",1));
-        commonCodeService.save(CommonCode.of("AUTH_GROUP","권한그룹","S0002","사용자 권한그룹",2));
-        commonCodeService.save(CommonCode.of("MENU_GROUP","메뉴그룹","SYSTEM_MANAGER","시스템 관리자 그룹",1));
-        commonCodeService.save(CommonCode.of("USER_ROLE","사용자 롤","SYSTEM_MANAGER","시스템 관리자 롤",2));
-        commonCodeService.save(CommonCode.of("MENU_GROUP","메뉴그룹","USER","사용자 그룹",2));
-        commonCodeService.save(CommonCode.of("DEL_YN","삭제여부","Y","삭제",2));
-        commonCodeService.save(CommonCode.of("USE_YN","사용여부","Y","사용",1));
+        commonCodeService.save(CommonCode.of("USER_STATUS", "계정상태", "ACCOUNT_LOCK", "잠김", 2));
+        commonCodeService.save(CommonCode.of("USER_ROLE", "사용자 롤", "API", "API 접근 롤", 6));
+        commonCodeService.save(CommonCode.of("USER_ROLE", "사용자 롤", "ASP_ACCESS", "관리시스템 접근 롤", 1));
+        commonCodeService.save(CommonCode.of("USER_ROLE", "사용자 롤", "ASP_MANAGER", "일반괸리자 롤", 3));
+        commonCodeService.save(CommonCode.of("LOCALE", "로케일", "en_US", "미국", 2));
+        commonCodeService.save(CommonCode.of("LOCALE", "로케일", "ko_KR", "대한민국", 1));
+        commonCodeService.save(CommonCode.of("DEL_YN", "삭제여부", "N", "미삭제", 1));
+        commonCodeService.save(CommonCode.of("USE_YN", "사용여부", "N", "사용안함", 2));
+        commonCodeService.save(CommonCode.of("USER_STATUS", "계정상태", "NORMAL", "활성", 1));
+        commonCodeService.save(CommonCode.of("AUTH_GROUP", "권한그룹", "S0001", "시스템관리자 그룹", 1));
+        commonCodeService.save(CommonCode.of("AUTH_GROUP", "권한그룹", "S0002", "사용자 권한그룹", 2));
+        commonCodeService.save(CommonCode.of("MENU_GROUP", "메뉴그룹", "SYSTEM_MANAGER", "시스템 관리자 그룹", 1));
+        commonCodeService.save(CommonCode.of("USER_ROLE", "사용자 롤", "SYSTEM_MANAGER", "시스템 관리자 롤", 2));
+        commonCodeService.save(CommonCode.of("MENU_GROUP", "메뉴그룹", "USER", "사용자 그룹", 2));
+        commonCodeService.save(CommonCode.of("DEL_YN", "삭제여부", "Y", "삭제", 2));
+        commonCodeService.save(CommonCode.of("USE_YN", "사용여부", "Y", "사용", 1));
 
 
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",1L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",2L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",3L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",4L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",5L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",6L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",7L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",8L,"Y","Y","N","N","N","N","N","N","N"));
-        authGroupMenuService.save(AuthGroupMenu.of("S0001",9L,"Y","N","N","N","N","N","N","N","N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 1L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 2L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 3L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 4L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 5L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 6L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 7L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 8L, "Y", "Y", "N", "N", "N", "N", "N", "N", "N"));
+        authGroupMenuService.save(AuthGroupMenu.of("S0001", 9L, "Y", "N", "N", "N", "N", "N", "N", "N", "N"));
 
     }
 
