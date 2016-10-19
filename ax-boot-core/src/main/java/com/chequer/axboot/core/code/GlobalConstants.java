bd5183d5caf95cd240be4e9c4a5ed166a6eee80f
Brant Hwang <dlstj3039@gmail.com>
2016-10-20 00:39:36 +0900

    Cookie Token 변경

--- ax-boot-core/src/main/java/com/chequer/axboot/core/code/GlobalConstants.java
+++ ax-boot-core/src/main/java/com/chequer/axboot/core/code/GlobalConstants.java
@@ -6,7 +6,7 @@ public class GlobalConstants {
 
     public static final String CORE_PACKAGE = "com.chequer.axboot.core";
 
-    public static final String LAST_NAVIGATED_PAGE = "c_h_e_l_n_p_g";
+    public static final String LAST_NAVIGATED_PAGE = "a_x_b_l_n_p";
 
-    public static final String ADMIN_AUTH_TOKEN_KEY = "c_h_e_q_a_t_k";
+    public static final String ADMIN_AUTH_TOKEN_KEY = "a_x_b_a_a_t_k";
 }
