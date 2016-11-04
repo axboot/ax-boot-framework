b423edfd97ef8c6d80f280bbc39957e11c37ffd9
Brant Hwang <dlstj3039@gmail.com>
2016-11-04 15:38:28 +0900

    update core

--- ax-boot-core/src/main/java/com/chequer/axboot/core/controllers/UtilController.java
+++ ax-boot-core/src/main/java/com/chequer/axboot/core/controllers/UtilController.java
@@ -3,6 +3,7 @@ package com.chequer.axboot.core.controllers;
 import com.chequer.axboot.core.utils.CoreUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,7 +12,7 @@ import java.util.Map;
 @RestController
 public class UtilController extends BaseController {
 
-    @RequestMapping(value = "/api/v1/utils/uuid")
+    @RequestMapping(value = "/api/v1/utils/uuid", method = RequestMethod.GET)
     public Map<String, Object> uuid() {
         Map<String, Object> map = new HashMap<>();
         map.put("uuid", CoreUtils.getUUID());
