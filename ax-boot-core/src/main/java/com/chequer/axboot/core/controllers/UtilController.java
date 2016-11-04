cb6a353ec32a64df7d1fde3b7c05f498a9931752
Brant Hwang <dlstj3039@gmail.com>
2016-11-04 15:00:11 +0900

    UUID Util 추가

---
+++ ax-boot-core/src/main/java/com/chequer/axboot/core/controllers/UtilController.java
@@ -0,0 +1,21 @@
+package com.chequer.axboot.core.controllers;
+
+import com.chequer.axboot.core.utils.CoreUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UtilController extends BaseController {
+
+    @RequestMapping(value = "/api/v1/utils/uuid")
+    public Map<String, Object> uuid() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("uuid", CoreUtils.getUUID());
+        return map;
+    }
+}
+
--- ax-boot-initialzr/src/main/java/com/chequer/axboot/initialzr/service/ProjectGenerator.java
+++ ax-boot-initialzr/src/main/java/com/chequer/axboot/initialzr/service/ProjectGenerator.java
@@ -52,7 +52,7 @@ public class ProjectGenerator {
         values.put("description", description);
         values.put("groupId", groupId);
         values.put("sessionCookie", uuid);
-        values.put("axbootCoreVersion", "2.1.11");
+        values.put("axbootCoreVersion", "2.1.13");
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
