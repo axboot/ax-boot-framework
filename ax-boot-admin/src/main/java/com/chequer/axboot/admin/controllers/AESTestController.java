60f772395308d65d164c100401e527b0adc6fbd5
Brant Hwang <dlstj3039@gmail.com>
2017-02-24 11:56:11 +0900

    base64 포함

--- ax-boot-admin/src/main/java/com/chequer/axboot/admin/controllers/AESTestController.java
+++ ax-boot-admin/src/main/java/com/chequer/axboot/admin/controllers/AESTestController.java
@@ -1,6 +1,7 @@
 package com.chequer.axboot.admin.controllers;
 
 import com.chequer.axboot.core.controllers.BaseController;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,16 +27,28 @@ public class AESTestController extends BaseController {
 
     @RequestMapping(value = "/api/v1/aes/enc", method = RequestMethod.POST)
     public String enc(HttpServletRequest request) throws NoSuchPaddingException, InvalidAlgorithmParameterException, IOException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        return aes256Util.aesEncode(IOUtils.toString(request.getInputStream(), "UTF-8"));
+        String data = IOUtils.toString(request.getInputStream(), "UTF-8");
+        return encode(aes256Util.aesEncode(data));
     }
 
     @RequestMapping(value = "/api/v1/aes/dec", method = RequestMethod.POST)
     public String dec(HttpServletRequest request) throws NoSuchPaddingException, InvalidAlgorithmParameterException, IOException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        return aes256Util.aesDecode(IOUtils.toString(request.getInputStream(), "UTF-8"));
+        String data = IOUtils.toString(request.getInputStream(), "UTF-8");
+        return aes256Util.aesDecode(decode(data));
+    }
+
+    public String decode(String data) throws UnsupportedEncodingException {
+        byte[] byteStr = Base64.decodeBase64(data.getBytes());
+        return new String(byteStr, "UTF-8");
+    }
+
+    public String encode(String data) throws UnsupportedEncodingException {
+        byte[] byteStr = Base64.encodeBase64(data.getBytes());
+        return new String(byteStr, "UTF-8");
     }
 
     public static void main(String[] args) throws UnsupportedEncodingException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         AES256Util aes256Util = new AES256Util("l25232trf6104z3439q8zo6zix8k6s39");
-        System.out.println(aes256Util.aesDecode("bndLRk93dE8xdmNQQThBM1gzc3ZXRndscjArM0E3V3loZGk2Um12U1Nqb1piOW1YNE4wNnMreFF0K2NBdjVQOVdMaXN0a1BxZlNWVnl3T3VkdXhTK1E9PQ=="));
+        System.out.println(aes256Util.aesDecode(new String(Base64.decodeBase64("bndLRk93dE8xdmNQQThBM1gzc3ZXRndscjArM0E3V3loZGk2Um12U1Nqb1piOW1YNE4wNnMreFF0K2NBdjVQOVdMaXN0a1BxZlNWVnl3T3VkdXhTK1E9PQ=="), "UTF-8")));
     }
 }
