ddf345870f879e2ba4bdeae9f863bced50bce476
Brant Hwang <dlstj3039@gmail.com>
2017-05-04 09:18:59 +0900

    Update Initializr

--- ax-boot-initialzr/src/main/java/com/chequer/axboot/initialzr/service/ProjectGenerator.java
+++ ax-boot-initialzr/src/main/java/com/chequer/axboot/initialzr/service/ProjectGenerator.java
@@ -52,7 +52,7 @@ public class ProjectGenerator {
         values.put("description", description);
         values.put("groupId", groupId);
         values.put("sessionCookie", uuid);
-        values.put("axbootCoreVersion", "2.1.31");
+        values.put("axbootCoreVersion", "2.1.33");
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
